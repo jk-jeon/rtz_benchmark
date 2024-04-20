d9a32e72e62443e4ed0bb0e78027217462bb169d
Junekey Jeon <j6jeon@ucsd.edu>
2024-04-20 13:34:06 -0700

    Trim some stuffs

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
-#include <string>
 
-#include "lib.hpp"
-
-auto main() -> int
+int main()
 {
-  library lib;
-  std::string message = "Hello from " + lib.name + "!";
-  std::cout << message << '\n';
-  return 0;
+
 }
